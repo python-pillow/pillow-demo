23ce2ecca1b66dbb12012a89d46d729ee18b45bd
Jeffrey A. Clark (Alex) <aclark@aclark.net>
2024-04-03 18:28:31 -0400

    Add reveal

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import 'reveal.js/dist/reveal.css';
+import 'reveal.js/dist/theme/black.css';
+import Reveal from 'reveal.js';
+import RevealNotes from 'reveal.js/plugin/notes/notes.js';
+Reveal.initialize({ slideNumber: true, plugins: [ RevealNotes ]});
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css',
+    }),
+  ],
+};
