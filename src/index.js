d36038471b9fec99e2eba480f27ca351122fdb0b
Jeffrey A. Clark (Alex) <aclark@aclark.net>
2024-07-07 14:22:15 -0400

    Revert "Prep for Austin Py"
    This reverts commit 0f62f795777be7c620b6f3ce5a37b5c1be724572.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import 'reveal.js/dist/reveal.css';
 import 'reveal.js/dist/theme/black.css';
 import Reveal from 'reveal.js';
 import RevealNotes from 'reveal.js/plugin/notes/notes.js';
-import RevealHighlight from 'reveal.js/plugin/highlight/highlight.js';
-Reveal.initialize({ slideNumber: true, plugins: [ RevealNotes, RevealHighlight ]});
+Reveal.initialize({ slideNumber: true, plugins: [ RevealNotes ]});
