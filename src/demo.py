0361f48e92764968170a1c3861d7367f9ebc174a
Jeffrey A. Clark (Alex) <aclark@aclark.net>
2024-07-09 19:52:06 -0400

    Prep for Austin Py

--- src/demo.py
+++ src/demo.py
@@ -473,6 +473,7 @@ code.interact(local=globals(), readfunc=readfunc)
 # https://pillow.readthedocs.io/en/stable/handbook/concepts.html#coordinate-system
 # ===============================================================================
 console.print(Rule("[bold magenta]Example #34[/bold magenta]"))
+import cart  # noqa
 console.print(Rule())
 code.interact(local=globals(), readfunc=readfunc)
 
@@ -481,6 +482,7 @@ code.interact(local=globals(), readfunc=readfunc)
 # https://pillow.readthedocs.io/en/stable/handbook/concepts.html#palette
 # ===============================================================================
 console.print(Rule("[bold magenta]Example #35[/bold magenta]"))
+import pal  # noqa
 console.print(Rule())
 code.interact(local=globals(), readfunc=readfunc)
 
@@ -511,6 +513,7 @@ code.interact(local=globals(), readfunc=readfunc)
 # https://pillow.readthedocs.io/en/stable/handbook/concepts.html#transparency
 # ===============================================================================
 console.print(Rule("[bold magenta]Example #36[/bold magenta]"))
+import alpha  # noqa
 console.print(Rule())
 code.interact(local=globals(), readfunc=readfunc)
 
