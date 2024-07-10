6114fda7f05d32bdadecce9afae9a981b0716ae3
Jeffrey A. Clark (Alex) <aclark@aclark.net>
2024-07-10 06:59:25 -0400

    Prep for Austin Py

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import 'reveal.js/dist/reveal.css';
 import 'reveal.js/dist/theme/black.css';
 import Reveal from 'reveal.js';
 import RevealNotes from 'reveal.js/plugin/notes/notes.js';
-Reveal.initialize({ autoSlide: 300, slideNumber: true, plugins: [ RevealNotes ]});
+Reveal.initialize({ autoSlide: 0, slideNumber: true, plugins: [ RevealNotes ]});
