1c741bff150a759feaf14b0dee363168e42e1d03
Jeffrey A. Clark (Alex) <aclark@aclark.net>
2024-07-20 14:16:04 -0400

    Update demo

--- src/ps.py
+++ src/ps.py
@@ -1,4 +1,4 @@
-from PIL import PSDraw
+from PIL import Image, PSDraw
 
 # Define the PostScript file
 ps_file = open("output.ps", "wb")
@@ -10,7 +10,7 @@ ps = PSDraw.PSDraw(ps_file)
 ps.begin_document()
 
 # Set the text to be drawn
-text = "Hello, this is a test text!"
+text = "Hopper"
 
 # Define the PostScript font
 font_name = "Helvetica-Narrow-Bold"
@@ -25,6 +25,20 @@ text_height = font_size
 page_width, page_height = 595, 842  # A4 size in points
 text_x = (page_width - text_width) // 2
 text_y = page_height - text_height - 50  # Distance from the top of the page
+
+# Load the image
+image_path = "img/hopper.ppm"  # Update this with your image path
+with Image.open(image_path) as im:
+    # Resize the image if it's too large
+    im.thumbnail((page_width - 100, page_height // 2))
+
+    # Define the box where the image will be placed
+    img_width, img_height = im.size
+    img_x = (page_width - img_width) // 2
+    img_y = text_y + text_height - 200  # 200 points below the text
+
+    # Draw the image (75 dpi)
+    ps.image((img_x, img_y, img_x + img_width, img_y + img_height), im, 75)
 
 # Draw the text
 ps.setfont(font_name, font_size)
